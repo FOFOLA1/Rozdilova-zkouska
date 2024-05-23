527afbb1c5904e82d3aeabd40ecd8301c3efd2b5
Filip Rehacek <filiprehacek4@gmail.com>
2024-05-23 19:50:45 +0200

    Vytvořena logika na hledání kombinací, jednoduchý výpis (zatím nefunkční)

--- Jizdenka/Program.cs
+++ Jizdenka/Program.cs
@@ -1,17 +1,18 @@
-﻿namespace Jizdenka
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Jizdenka
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            int rows = 3;
-            int cols = 3;
-            int holes = 3;
+            int rows = 2;
+            int cols = 2;
+            int holes = 2;
 
-            int[] comb = new int[holes];
             List<int[]> combs = new List<int[]> ();
-            //Array<int[]> combs = new int[holes];
 
 
             //do
@@ -36,25 +37,85 @@
                 jiz[i] = i + 1;
             }
 
+            combinations(jiz, new int[holes], 0, jiz.Length - 1, 0, holes, combs);
+            string txt = formatJiz(combs, rows, cols);
+            Console.WriteLine(txt);
 
-            for (int i = 0; i < jiz.Length-holes+1; i++)
+        }
+
+        static void combinations(int[] jiz, int[] comb, int start, int len, int index, int holes, List<int[]> combs)
+        {
+
+            if (index == holes)
             {
-                comb[0] = i;
-                
-                for (int j = i+1; j < jiz.Length-holes+2; j++)
+                int[] temp = new int[holes];
+                for (int j = 0; j < holes; j++)
                 {
-                    comb[1] = j;
-                    for (int k = j+1; k < jiz.Length-holes+3; k++)
-                    {
-                        comb[2] = k;
-                        combs.Add(comb);
-                    }
+                    temp[j] = comb[j];
                 }
-
+                combs.Add(temp);
+                return;
             }
 
+            for (int i = start; i <= len && len - i + 1 >= holes - index; i++)
+            {
+                comb[index] = jiz[i];
+                combinations(jiz, comb, i + 1, len, index + 1, holes, combs);
+            }
+        }
 
+        static string formatJiz(List<int[]> combs, int rows, int cols)
+        {
+            int maxlen = (rows * cols).ToString().Length;
+            int count = 0;
+            int temp;
 
-        }    
+            string betwline = "";
+            for (int i = 0; i < cols; i++)
+            {
+                betwline += " " + new string('-', maxlen);
+            }
+
+            string numline = "";
+            string num;
+            string str = "";
+            string strOut = "";
+
+            foreach (int[] item in combs)
+            {
+                count++;
+                str = count + "\n";
+
+                str += betwline + "\n";
+
+                for (int i = 1; i <= (cols * rows); i++)
+                {
+                    if (i != cols) temp = i;
+                    else temp = i%cols;
+
+                    if (item.Any(cislo => cislo == temp))
+                    {
+                        numline += "|" + new string('\u2588', maxlen);
+                    }
+                    else
+                    {
+                        num = String.Format("{0," + maxlen + "}", i);
+                        numline += "|" + num;
+                    }
+                    
+
+                    if (i%cols == 0)
+                    {
+                        numline += "|\n" + betwline + "\n";
+                    }
+                }
+                str += numline + "\n";
+                strOut += str;
+            }
+            return strOut;
+        }
+
     }
+
+    // Program najde všechny možnosti správně, ale výpis nefunguje
 }
